210eb5fb81d189c9875c262525f3667e167bef35
19seniman <161638164+19seniman@users.noreply.github.com>
2025-07-29 23:23:17 +0700

    lim.js

--- lim.js
+++ lim.js
@@ -941,7 +941,7 @@ const configSubmitButton = blessed.button({
     setTimeout(() => {
       try {
         if (!isHeaderRendered) {
-          figlet.text("19Seniman", { font: "Doom" }, (err, data) => {
+          figlet.text("19Seniman", { font: "Def Leppard" }, (err, data) => {
             if (!err) headerBox.setContent(`{center}{bold}{cyan-fg}${data}{/cyan-fg}{/bold}{/center}`);
             isHeaderRendered = true;
           });
